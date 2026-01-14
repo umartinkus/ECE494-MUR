52aa5530e284b16954d32214c31da098cdf15e64
Nolan <nrmshaw100@gmail.com>
2026-01-13 19:53:03 -0700

    commiting before changing branches

--- ros2_ws/src/esp32_sensors/src/listener.cpp
+++ ros2_ws/src/esp32_sensors/src/listener.cpp
@@ -11,33 +11,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
-
-class DataPublisher : public rclcpp::Node
-{
-public:
-	DataPublisher() : Node("data_publisher"), count_(0) {
-		publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-		timer_ = this->create_wall_timer(500ms, std::bind(&DataPublisher::timer_callback, this));
-	}
-
-	void get_port_data(SerialPort port_obj) {
-
-	}
-
-private:
-	void timer_callback() {
-		auto message = std_msgs::msg::String();
-		message.data = "Hello, world! " + std::to_string(count_++);
-		RCLCPP_INFO(this->get_logger(), "Publishing: %s", message.data.c_str());
-		publisher_->publish(message);
-	}
-
-	rclcpp::TimerBase::SharedPtr timer_;
-	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-	size_t count_;
-};
-
 class SerialPort {
 public:
 	SerialPort() : port("/dev/ttyUSB0") {
@@ -62,11 +35,40 @@ private:
 	ssize_t bytes_read;
 };
 
+using namespace std::chrono_literals;
+
+class DataPublisher : public rclcpp::Node
+{
+public:
+	DataPublisher() : Node("data_publisher"), count_(0) {
+		publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+		timer_ = this->create_wall_timer(
+        std::chrono::miliseconds(500),
+        std::bind(&DataPublisher::timer_callback, this));
+	}
+
+	void get_port_data(SerialPort port_obj) {
+
+	}
+
+private:
+	void timer_callback() {
+		auto message = std_msgs::msg::String();
+		message.data = "Hello, world! " + std::to_string(count_++);
+		RCLCPP_INFO(this->get_logger(), "Publishing: %s", message.data.c_str());
+		publisher_->publish(message);
+	}
+
+	rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+	size_t count_;
+};
+
 int main(int argc, char * argv[])
 {
 	rclcpp::init(argc,argv);
-	// auto publisher = std::make_shared<DataPublisher>();
-	// rclcpp::spin(publisher);
+	auto publisher = std::make_shared<DataPublisher>();
+	rclcpp::spin(publisher);
 
 	SerialPort esp32;
 	esp32.open_port();
