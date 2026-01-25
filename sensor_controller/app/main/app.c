ae9578cc6a481cc5831ac06cc9bb0ce4bc0ad87e
umartinkus <blink@UriahsPC>
2026-01-25 13:13:43 -0700

    fixed merge conflict in app.c

--- sensor_controller/app/main/app.c
+++ sensor_controller/app/main/app.c
@@ -35,12 +35,7 @@ void app_main(void)
     depth_msg_buffer = xMessageBufferCreate(1024);
     data_buffers.pose_buff = (void*)pose_msg_buffer;
     data_buffers.depth_buff = (void*)depth_msg_buffer;
-<<<<<<< HEAD
-    xTaskCreatePinnedToCore(GET_POSE, "POSE_TASK_C0", 4096, (void*)pose_msg_buffer, TASK_PRIO_5, NULL, CORE0);
-    xTaskCreatePinnedToCore(UPDATE_GS, "UPDATE_GS_TASK_C0", 4096, (void*)&data_buffers, TASK_PRIO_4, NULL, CORE0);
-=======
     // xTaskCreatePinnedToCore(GET_POSE, "POSE_TASK_C0", 4096, (void*)pose_msg_buffer, TASK_PRIO_5, NULL, CORE0);
     // xTaskCreatePinnedToCore(UPDATE_GS, "UPDATE_GS_TASK_C0", 4096, (void*)&data_buffers, TASK_PRIO_4, NULL, CORE1);
     xTaskCreatePinnedToCore(THRUST_CTRL, "THRUST_CTRL_TASK_C0", 4096, NULL, TASK_PRIO_3, NULL, CORE0);
->>>>>>> pwm
 }
