1166cfd45b7a202afbbaea4e92db97cfee2ea0ae
Nolan <nrmshaw100@gmail.com>
2026-01-25 15:14:13 -0700

    added uart queue, added task for uart listening. Commented stuff for testing purposes

--- sensor_controller/app/main/app.c
+++ sensor_controller/app/main/app.c
@@ -30,26 +30,12 @@ static QueueHandle_t uart_event_queue;
 
 void app_main(void)
 {
-<<<<<<< HEAD
-    // start the uart port
-    init();
-
-    // create the buffers and queues
-    pose_msg_buffer = xMessageBufferCreate(1024);
-    depth_msg_buffer = xMessageBufferCreate(1024);
-    uart_event_queue = xQueueCreate(256, sizeof(uint8_t));
-    data_buffers.pose_buff = (void*)pose_msg_buffer;
-    data_buffers.depth_buff = (void*)depth_msg_buffer;
-    // xTaskCreatePinnedToCore(GET_POSE, "POSE_TASK_C0", 4096, (void*)pose_msg_buffer, TASK_PRIO_5, NULL, CORE0);
-    // xTaskCreatePinnedToCore(UPDATE_GS, "UPDATE_GS_TASK_C0", 4096, (void*)&data_buffers, TASK_PRIO_4, NULL, CORE1);
-    xTaskCreatePinnedToCore(THRUST_CTRL, "THRUST_CTRL_TASK_C0", 4096, (void*)uart_event_queue, TASK_PRIO_4, NULL, CORE0);
-    xTaskCreatePinnedToCore(UART_LISTEN, "UART_LISTEN_TASK_C0", 4096, (void*)uart_event_queue, TASK_PRIO_3, NULL, CORE0);
-=======
     slow_lane_buff = xMessageBufferCreate(1024);
     fast_lane_buff = xMessageBufferCreate(2048);
+    uart_event_queue = xQueueCreate(512, sizeof(uint8_t));
     uart_init();
-    xTaskCreatePinnedToCore(GET_POSE, "POSE_TASK_C0", 4096, (void*)slow_lane_buff, TASK_PRIO_5, NULL, CORE0);
-    xTaskCreatePinnedToCore(UPDATE_GS, "UPDATE_GS_TASK_C0", 4096, (void*)&fast_lane_buff, TASK_PRIO_4, NULL, CORE1);
-    // xTaskCreatePinnedToCore(THRUST_CTRL, "THRUST_CTRL_TASK_C0", 4096, NULL, TASK_PRIO_3, NULL, CORE0);
->>>>>>> refs/remotes/origin/sensor_controller
+    // xTaskCreatePinnedToCore(GET_POSE, "POSE_TASK_C0", 4096, (void*)slow_lane_buff, TASK_PRIO_5, NULL, CORE0);
+    // xTaskCreatePinnedToCore(UPDATE_GS, "UPDATE_GS_TASK_C0", 4096, (void*)&fast_lane_buff, TASK_PRIO_4, NULL, CORE1);
+    xTaskCreatePinnedToCore(THRUST_CTRL, "THRUST_CTRL_TASK_C0", 4096, (void*)uart_event_queue, TASK_PRIO_3, NULL, CORE0);
+    xTaskCreatePinnedToCore(UART_LISTEN, "UART_LISTEN_TASK_C0", 4096, (void*)uart_event_queue, TASK_PRIO_4, NULL, CORE0);
 }
