6dcc6060d417f7da762d738baea3a0a4e0b1e403
umartinkus <umartinkus@gmail.com>
2026-03-09 16:39:40 -0600

    added small error check to SPI init procedure

--- system_monitor/main/system_monitor.c
+++ system_monitor/main/system_monitor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "SENSOR.h"
+#include "COMMS.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/idf_additions.h"
 #include "freertos/task.h"
@@ -9,4 +10,5 @@
 void app_main(void)
 {
     xTaskCreate(SENSOR,"sensor_task", 4096, NULL, DEFAULT_PRIORITY, NULL);
+    xTaskCreate(COMMS, "comms_task", 4096, NULL, DEFAULT_PRIORITY, NULL);
 }
--- system_monitor/tasks/COMMS/COMMS.c
+++ system_monitor/tasks/COMMS/COMMS.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "COMMS.h"
+#include "spi.h"
+#include "esp_err.h"
 
 void COMMS(void *args)
 {
+    while(spi3_slave_init() != ESP_OK){
 
+    }
     for(;;){
         
     }
