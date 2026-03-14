26690ee38d57d3231afbd585d9e8c0a19b299f43
umartinkus <umartinkus@gmail.com>
2026-03-14 15:43:37 -0600

    wrote some template code for Nolan to integrate with his spi code

--- system_monitor/tasks/COMMS/COMMS.c
+++ system_monitor/tasks/COMMS/COMMS.c
@@ -2,13 +2,22 @@
 #include "COMMS.h"
 #include "spi.h"
 #include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "esp_log.h"
+#include "sys_common.h"
+
+const static char *TAG = "COMMS";
+static system_status_t sys_stat;
 
 void COMMS(void *args)
 {
-    while(spi3_slave_init() != ESP_OK){
+    // while(spi3_slave_init() != ESP_OK){
 
-    }
+    // }
     for(;;){
-        
+        get_system_status(&sys_stat); // accessing the global system state and copying it by value into sys_stat
+        ESP_LOGI(TAG, "LEAK1 STATUS: %s", error_code_to_string(sys_stat.leak1_status));
+        ESP_LOGI(TAG,"LEAK2 STATUS: %s", error_code_to_string(sys_stat.leak2_status));
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
